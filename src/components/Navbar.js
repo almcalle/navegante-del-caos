6630c2daa518d9e5abfaf1873a804c8b6f45d8e2
alfonso <alfmcalle@gmail.com>
2019-08-31 17:01:01 +0200

    Style changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              Navegante del Caos
             </Link>
             {/* Hamburger menu */}
             <div
