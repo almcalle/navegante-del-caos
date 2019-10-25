3e3475abd5fd2c2131811247667730c940a8e0ee
alfonso <alfmcalle@gmail.com>
2019-10-25 19:24:11 +0400

    Added Instagram Images to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
-import InstagramFeed from './InstagramFeed'
+import InstagramFeed from './Instagram/InstagramFeed'
 
 
 const Footer = class extends React.Component {
