4e78a5ce573d750ffc587ab03230835f63ad2595
alfonso <alfmcalle@gmail.com>
2019-10-26 01:12:08 +0400

    Update InstagramFeed.js

--- src/components/Instagram/InstagramFeed.js
+++ src/components/Instagram/InstagramFeed.js
@@ -76,7 +76,7 @@ export default class InstagramFeed extends Component {
  * Shuffles array in place.
  * @param {Array} a items An array containing the items.
  */
-function shuffle(a) {
+shuffle(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
       j = Math.floor(Math.random() * (i + 1));
