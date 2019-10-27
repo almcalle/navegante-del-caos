98e00e89c26790b3df14b206ff716ee079054bdf
alfonso <alfmcalle@gmail.com>
2019-10-27 23:23:33 +0400

    Update tags.js

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,63 +1,62 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import PreviewCompatibleImage from "../components/PreviewCompatibleImage"
+import React from "react";
+import Helmet from "react-helmet";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 class TagRoute extends React.Component {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges
-    const postLinks = posts.map(post =>  post.node.frontmatter.isPublished && (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-        <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.node.frontmatter.featuredpost ? "is-featured" : ""
-                }`}
-              >
-                <header>
-                  {post.node.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.node.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.title}`
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.node.fields.slug}
-                    >
-                      {post.node.frontmatter.title}
+    const posts = this.props.data.allMarkdownRemark.edges;
+    const postLinks = posts.map(
+      post =>
+        post.node.frontmatter.isPublished && (
+              <div className="is-parent column is-6" key={post.id}>
+                <article
+                  className={`blog-list-item tile is-child box notification ${
+                    post.node.frontmatter.featuredpost ? "is-featured" : ""
+                  }`}
+                >
+                  <header>
+                    {post.node.frontmatter.featuredimage ? (
+                      <div className="featured-thumbnail">
+                        <PreviewCompatibleImage
+                          imageInfo={{
+                            image: post.node.frontmatter.featuredimage,
+                            alt: `featured image thumbnail for post ${post.title}`
+                          }}
+                        />
+                      </div>
+                    ) : null}
+                    <p className="post-meta">
+                      <Link
+                        className="title has-text-primary is-size-4"
+                        to={post.node.fields.slug}
+                      >
+                        {post.node.frontmatter.title}
+                      </Link>
+                      <span> &bull; </span>
+                      <span className="subtitle is-size-5 is-block">
+                        {post.node.frontmatter.date}
+                      </span>
+                    </p>
+                  </header>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button" to={post.node.fields.slug}>
+                      Seguir Leyendo →
                     </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.node.frontmatter.date}
-                    </span>
                   </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.node.fields.slug}>
-                    Seguir Leyendo →
-                  </Link>
-                </p>
-              </article>
-            </div>
-        </Link>
-      </li>
-    ))
-    const tag = this.props.pageContext.tag
-    const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
+                </article>
+              </div>
+        )
+    );
+    const tag = this.props.pageContext.tag;
+    const title = this.props.data.site.siteMetadata.title;
+    const totalCount = this.props.data.allMarkdownRemark.totalCount;
     const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } etiquetados con “${tag}”`
+      totalCount === 1 ? "" : "s"
+    } etiquetados con “${tag}”`;
 
     return (
       <Layout>
@@ -67,10 +66,10 @@ class TagRoute extends React.Component {
             <div className="columns">
               <div
                 className="column is-10 is-offset-1"
-                style={{ marginBottom: '6rem' }}
+                style={{ marginBottom: "6rem" }}
               >
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-                <ul className="taglist">{postLinks}</ul>
+                <div className="columns is-multiline">{postLinks}</div>
                 <p>
                   <Link to="/tags/">Browse all tags</Link>
                 </p>
@@ -79,11 +78,11 @@ class TagRoute extends React.Component {
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
 
-export default TagRoute
+export default TagRoute;
 
 export const tagPageQuery = graphql`
   query TagPage($tag: String) {
@@ -120,4 +119,4 @@ export const tagPageQuery = graphql`
       }
     }
   }
-`
+`;
